7372bc0caa044be2b09295e30768eb43f556e20a
smzajac <sarahmzajac@gmail.com>
2017-08-22 16:09:35 -0400

    robot edits

--- robots.js
+++ robots.js
@@ -2,29 +2,46 @@ const express= require('express');
 const path= require('path');
 const mustacheExpress = require('mustache-express');
 const app=express();
+const data = require("./data.js");
 
 // const data = require('data.js');
 
 app.engine('mustache', mustacheExpress());
 // app.set('views', './views')
 app.set('view engine', 'mustache')
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, '/views'));
+app.use(express.static('public'));
 
-app.get('/', function(req,res){
-  res.send("Hello, World!");
+// app.get('/', function(req,res){
+//   res.send("Hello, World!");
+// });
+
+app.get('/', function (req, res, next) {
+  res.render('index', {users: data.users})
 });
 
-let data = "hello";
 
-app.get('/todo', function (req, res) {
-  res.render("index", data);
-})
+// app.get('/todo', function (req, res) {
+//   res.render("index", data);
+// })
+
+// app.get('/users/:id', function(req, res){
+//   let id = req.params.id;
+//   let user = data.users[id];
+//   res.render('user', user);
+// });
 
 app.get('/users/:id', function(req, res){
   let id = req.params.id;
-  let user = data.users[id];
+  let actualID = id - 1;
+  let user = data.users[actualID];
+
   res.render('user', user);
 });
+
+// app.get('/user/:id', function(req, res){
+//     res.send('user ' + req.params.id);
+// });
 
 app.listen(3000, function () {
   console.log('Successfully started express application!');
